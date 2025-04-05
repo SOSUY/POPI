fe691a4ca3c77ecc59bc59ff16e3aabe7cb8d83f
SOSUY <sanek21rusg@gmail.com>
2025-04-05 15:30:51 +0300

    Update masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,51 @@
-def mask_card(card_number: str) -> str:
-    """
-    Функция принимает на вход номер карты и возвращает ее маску.
-    """
-    if len(card_number) == 16:  # Проверяем длину номера карты
-        masked_card = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-        return masked_card
-    return "Некорректный номер карты"
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("Python-Project/logs/masks.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
-def mask_account(account_number: str) -> str:
-    """
-    Функция принимает на вход номер счета и возвращает его маску.
-    """
-    if len(account_number) > 4:  # Проверяем, что номер счета хотя бы 4 символа
-        masked_account = "**" + account_number[-4:]
-        return masked_account
-    return "Некорректный номер счета"
+def get_mask_card_number(number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску, номер карты замаскирован."""
+    card_number = ""
+    if len(number) != 16:
+        logger.error("Недопустимое количество символов")
+        return "Error"
+    for num in range(len(number)):
+        if number[num].isalpha():
+            logger.error("Нужны численные значения")
+            return "Error"
+    else:
+        for num in range(len(number)):
+            if number[num].isalpha() or number[num] == " ":
+                pass
+            else:
+                card_number += number[num]
+        correct_number = card_number[0:7] + card_number[7:14] + card_number[14:19]
+        number_mask = (
+            correct_number[0:4] + " " + correct_number[4:6] + "**" + " " + "****" + " " + correct_number[12:16]
+        )
+        logger.info(f"функция закончила выполнение со значениямм {number_mask}")
+        return number_mask
+
+
+def get_mask_account(number: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску, номер счета замаскирован."""
+    if len(number) < 20:
+        logger.error("Символов меньше чем 20")
+        return "Error"
+    for num in range(len(number)):
+        if number[num].isalpha():
+            logger.error("Нужны численные значения")
+            return "Error"
+    else:
+        number_mask = "**" + number[-4:]
+        logger.info(f"функция закончила выполнение со значениями {number_mask}")
+        return number_mask
+
+
+# get_mask_account("14234354436141345")
+# get_mask_card_number("112222333344455")
