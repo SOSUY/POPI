1afc9038bd8121be32a1e476e94c6ff9217dadce
SOSUY <sanek21rusg@gmail.com>
2025-04-08 13:38:57 +0300

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,9 +4,9 @@ from src.utils import json_transactions
 
 
 def test_json_transactions(test_json):
-    mock_way = Mock(return_value=r"../Python-Project/data/operations.jso")
-    test_way = r"../Python-Project/data/operations.json"
-    not_correct_way = r"../Python-Project/tests/test_operations.json"
+    mock_way = Mock(return_value=r"../POPI-main/data/operations.jso")
+    test_way = r"../POPI-main/data/operations.json"
+    not_correct_way = r"../POPI-main/tests/test_operations.json"
     assert json_transactions(mock_way) == []
     assert json_transactions(test_way) == test_json
     assert json_transactions(not_correct_way) == []
